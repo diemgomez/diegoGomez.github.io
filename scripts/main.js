b8872259c4f91ef68feb7dc3454830bdb261bedc
Diego <zemog150790@gmail.com>
2019-06-24 08:13:02 -0500

    agregando archivos a mi repositorio

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var miTitulo = document.querySelector('h1');
+miTitulo.innerHTML = 'Hello world';
+
+var miImage = document.querySelector('img');
+
+miImage.onclick = function() {
+  var miSrc = miImage.getAttribute('src');
+  if (miSrc === 'images/firefox-icon.png') {
+    miImage.setAttribute('src', 'images/firefox2.png');
+  } else {
+    miImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+};
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector('h1');
+
+function estableceNombreUsuario() {
+  var miNombre = prompt('Por favor, ingresa tu nombre.');
+  localStorage.setItem('nombre', miNombre);
+  miTitulo.textContent = 'Mozilla es fresco, ' + miNombre;
+  if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+  } else {
+    var nomberAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es fresco, ' + nomberAlmacenado;
+  }
+}
+
+miBoton.onclick = function() {
+  estableceNombreUsuario();
+};
